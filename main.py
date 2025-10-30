a106aac99d6441bee46c528c541d4ac029cc1a53
Umbhorn Ungkulpasvich <umbhorn.ung@gmail.com>
2025-10-30 09:23:43 +0900

    update the main

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ def main():
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."'
+        model='gemini-2.0-flash-001', 
+        contents="""Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."""
     )
     print(response.text)
     
     if response is None or response.usage_metadata is None: 
-        print("No usage metadata available.")
+        print("response is malformed")
         return
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     
-if __name__ == "__main__":
-    main()
+
+main()
