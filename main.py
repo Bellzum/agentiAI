3709dfdeef3abc61fb18e454aee823b4e530f340
Umbhorn Ungkulpasvich <umbhorn.ung@gmail.com>
2025-11-10 13:26:32 +0900

    update: still failing verbose

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
+    verbose_flag = False
     if len(sys.argv) < 2:
         print("I need a prompt!")
         sys.exit(1)
+    prompt =  sys.argv[1]
+    
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
     prompt =  sys.argv[1]
     # response = client.models.generate_content(model="gemini-2.0-flash-001",contents=prompt,)
    
@@ -26,8 +31,11 @@ def main():
     if response is None or response.usage_metadata is None: 
         print("response is malformed")
         return
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if verbose_flag:
+        print(f"user prompt: {prompt}")    
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     
 
 main()
